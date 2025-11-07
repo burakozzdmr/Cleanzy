6a2d0ed505713ef01aaf720eff0479bee657ddb3
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-11-07 16:06:40 +0300

    Initial Commit

---
+++ Cleanzy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleanzy
+//
+//  Created by Burak Özdemir on 7.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
